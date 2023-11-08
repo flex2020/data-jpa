91bb88c1add9bacfd2e7af12a3e60860ea237446
NOH UIBIN <hosan_1@naver.com>
2023-11-08 21:38:34 +0900

    JPA NamedQuery

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,6 +3,9 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     // MemberRepository는 인터페이스로 생성해야함
+    List<Member> findByUserNameAndAgeGreaterThan(String username, int age);
 }
--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -1,7 +1,5 @@
 package study.datajpa.repository;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +7,9 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 @SpringBootTest
@@ -17,25 +17,68 @@ import static org.junit.jupiter.api.Assertions.*;
 @Rollback(false)
 class MemberJpaRepositoryTest {
 
-    @Autowired MemberJpaRepository memberJpaRepository;
+    @Autowired MemberJpaRepository memberRepository;
+    @Autowired MemberRepository memberRepository2;
 
     @Test
     public void testMember() {
         Member member = new Member("memberA");
         // DB에 member 저장하고, saveMember로 가져옴
-        Member saveMember = memberJpaRepository.save(member);
+        Member saveMember = memberRepository.save(member);
 
         // DB에 저장한 member의 Id를 통해 해당 member를 찾아서 findMember로 가져옴
-        Member findMember = memberJpaRepository.find(saveMember.getId());
+        Member findMember = memberRepository.find(saveMember.getId());
 
         // findMember의 id와 member의 id가 같은지 테스트
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getId()).isEqualTo(member.getId());
 
         // findMember의 userName과 member의 userName이 같은지 테스트
-        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+        assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
 
         // findMember와 member가 같은지 테스트
         // JPA는 내가 save한 데이터(member)와 찾은 데이터(findMember)가 같다는 보장을 해줌 (1차 캐시)
-        Assertions.assertThat(findMember).isEqualTo(member);
+        assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // 단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //findMember1.setUserName("member123123123"); // 변경 감지 = 더티 체킹, update 역할 수행
+
+        // 리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+        // 카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+        long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
+    }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThen() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository2.findByUserNameAndAgeGreaterThan("AAA", 15);
+        assertThat(result.get(0).getUserName()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
     }
 }
